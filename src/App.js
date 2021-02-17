95fce45f9deca193cbe893eedcdd2551e0d16245
prince <prince.john@global-infonet.com>
2021-02-17 16:10:13 +0530

    myproject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// function App() {
+//   return (
+//     <div className="app">
+      
+//       <h1>hi</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { Component } from 'react';
+import './App.css';
+import Calendar from './components/calendar'
+
+const style ={
+  position: "relative",
+  margin: "50px auto"
+}
+
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <Calendar style={style} width="302px" />
+      </div>
+    );
+  }
 }
 
 export default App;
